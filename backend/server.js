ac2414371020f7b7df1397ecdfbea29cfadd8afc
Harry <ichiengbang@gmail.com>
2022-09-19 09:40:00 -0500

    server update

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(express.json()) 
 
 app.use("/api/v1/restaurants", restaurants) //api route, version
-app.use("*", (req, res) => res.status(404).json({ error("not found" })
+app.use("*", (req, res) => res.status(404).json({ error: "not found}"}))
 
 export default app //export this
